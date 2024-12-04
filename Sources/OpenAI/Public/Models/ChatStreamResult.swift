3d89c02dd939bc634f1d00c8dcbddbffe02e676f
naveennaidu <naveen@zeitalabs.com>
2024-12-04 12:58:54 +0530

    make id chatstreamresult optional

--- Sources/OpenAI/Public/Models/ChatStreamResult.swift
+++ Sources/OpenAI/Public/Models/ChatStreamResult.swift
@@ -117,7 +117,7 @@ public struct ChatStreamResult: Codable, Equatable {
     }
 
     /// A unique identifier for the chat completion. Each chunk has the same ID.
-    public let id: String
+    public let id: String?
     /// The object type, which is always `chat.completion.chunk`.
     public let object: String
     /// The Unix timestamp (in seconds) of when the chat completion was created.
